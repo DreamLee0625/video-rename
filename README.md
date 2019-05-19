54c0d2f781e61c586eb7682d399e4dd308f21ce8
Xiang Li <lixiang_cn@foxmail.com>
2019-05-19 11:15:13 +0800

    add conversion function

--- README.md
+++ README.md
@@ -3,17 +3,29 @@
 rename video_files according to create date,  
 also can create some extern string.  
 such:  
-20190325-121500.MP4  
-20190325-121500-trip.MP4  
+DJI0001.MP4 -> 20190325-121500.MP4  
+DJI0001.MP4 -> 20190325-121500-trip.MP4  
+DJI0001.MOV -> 20190325-121500-trip.MP4  
 
-env requires: python3 on windows/macOS
+env requires: python3 on windows/macOS  
+V1 and V2 scripts are independent of each other  
+requires moviepy in rename-video-v2.py  
 
 ## Usage
 for rename one video:  
 ```
-python rename-video.py -i video_path [-s some_extern_string]
+python rename-video-v1.py -i video_path [-s some_extern_string]
 ```
 for rename videos of one folder:
 ```
-python rename-video.py -d -i video_dir [-s some_extern_string]
+python rename-video-v1.py -d -i video_dir [-s some_extern_string]
+```
+And format conversion function is provided in V2 script:
+for rename and conversion one video:  
+```
+python rename-video-v2.py -i video_path [-s some_extern_string] [-f new_format, such '.mp4']
+```
+for rename and conversion videos of one folder:
+```
+python rename-video-v2.py -d -i video_dir [-s some_extern_string] [-f new_format, such '.mp4']
 ```
--- rename-video-v2.py
+++ rename-video-v2.py
@@ -16,10 +16,11 @@ parser = argparse.ArgumentParser(description="default: rename for video_file, en
 parser.add_argument('--input', '-i', help='input file or dir')
 parser.add_argument('--isDir', '-d', action='store_true', help='rename for video_dir') 
 parser.add_argument('--ext_str', '-s', default=None, help='ext string in new_name')
+parser.add_argument('--out_format', '-f', default=None, help='trans format of new video file')
 args = parser.parse_args()
 
 
-def process_file(video_file, ext_str=None):
+def process_file(video_file, ext_str=None, out_format=None):
     ### video_path, video_file, video_ext
     video_path, video_name = os.path.split(video_file)
     video_name, video_ext = os.path.splitext(video_name)
@@ -33,22 +34,28 @@ def process_file(video_file, ext_str=None):
     ctime = time.localtime(ctime)
     time_str = time.strftime("%Y%m%d-%H%M%S", ctime)
     ### new name
+    if out_format is not None:
+        video_ext = out_format
     if ext_str:
         new_name = "{}.{}{}".format(time_str, ext_str, video_ext)
     else:
         new_name = time_str + video_ext
     new_file = os.path.join(video_path, new_name)
     ### rename
-    try:
-        os.rename(video_file, new_file)
-    except:
-        print("[{}] rename err.".format(video_file), file=sys.stderr)
-        return False
+    if out_format is None:
+        try:
+            os.rename(video_file, new_file)
+        except:
+            print("[{}] rename err.".format(video_file), file=sys.stderr)
+            return False
+    else:
+        vfc = VideoFileClip(video_file)
+        vfc.write_videofile(filename=new_file, remove_temp=False)
     print("[{}] rename done.".format(video_file), file=sys.stderr)    
     return True
 
 
-def process_batch(video_dir, ext_str=None):
+def process_batch(video_dir, ext_str=None, out_format=None):
     count = 0
     err_count = 0
     for video_name in os.listdir(video_dir):
@@ -57,7 +64,7 @@ def process_batch(video_dir, ext_str=None):
             continue
         count += 1
         video_file = os.path.join(video_dir, video_name+video_ext)
-        res = process_file(video_file, ext_str=ext_str)
+        res = process_file(video_file, ext_str=ext_str, out_format=out_format)
         if not res:
             err_count += 1
     print("total: [{}], err: [{}]".format(count, err_count), file=sys.stderr)
@@ -66,7 +73,8 @@ def process_batch(video_dir, ext_str=None):
 if __name__ == "__main__":
     ext_str = args.ext_str
     in_data = args.input
+    out_format = args.out_format
     if args.isDir:
-        process_batch(in_data, ext_str)
+        process_batch(in_data, ext_str, out_format)
     else:
-        process_file(in_data, ext_str)
+        process_file(in_data, ext_str, out_format)
